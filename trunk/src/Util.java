5ca7cb95a9b770f8734bd0aecf1f7cb8df2f37aa
mbfs.pj <mbfs.pj@7a408387-9585-1bdd-a7f5-fc896439e68e>
2011-01-02 22:05:41 +0000

    poprawa kodowania

--- trunk/src/Util.java
+++ trunk/src/Util.java
@@ -17,29 +17,34 @@ import mess.Message;
 
 public class Util {
 	/**
-	 * Tworzy czyteln± reprezentacjê napisow± dla danej tabeli dwuwymiarowej.
-	 *
-	 * @param tableRows   Dwuwymiarowa lista reprezentuj±ca tabelê danych.
-	 * @return            Reprezentacja napisowa podanej tabeli.
-	 * @throws IllegalArgumentException je¿eli ilo¶ci elementów w wierszach
-	 *                    nie s± równe.
+	 * Tworzy czytelnÄ… reprezentacjÄ™ napisÃ³w dla danej tabeli dwuwymiarowej.
+	 * 
+	 * @param tableRows
+	 *            Dwuwymiarowa lista reprezentujÄ…ca tabelÄ™ danych.
+	 * @return Reprezentacja napisowa podanej tabeli.
+	 * @throws IllegalArgumentException
+	 *             jeÅ¼eli iloÅ›ci elementÃ³w w wierszach nie sÄ… rÃ³wne.
 	 */
-	public static <T> String tableToString(List<List<T>> tableRows) {
+	public static <T> String tableToString(final List<List<T>> tableRows) {
 		return tableToString(null, tableRows);
 	}
+
 	/**
-	 * Tworzy czyteln± reprezentacjê napisow± dla danej tabeli dwuwymiarowej.
-	 *
-	 * @param columnNames Nazwy kolumn.
-	 * @param tableRows   Dwuwymiarowa lista reprezentuj±ca tabelê danych.
-	 * @return            Reprezentacja napisowa podanej tabeli.
-	 * @throws IllegalArgumentException je¿eli ilo¶æ nazw kolumn oraz ilo¶ci
-	 *                    elementów w wierszach nie s± równe.
+	 * Tworzy czytelnÄ… reprezentacjÄ™ napisÃ³w dla danej tabeli dwuwymiarowej.
+	 * 
+	 * @param columnNames
+	 *            Nazwy kolumn.
+	 * @param tableRows
+	 *            Dwuwymiarowa lista reprezentujÄ…ca tabelÄ™ danych.
+	 * @return Reprezentacja napisowa podanej tabeli.
+	 * @throws IllegalArgumentException
+	 *             jeÅ¼eli iloÅ›Ä‡ nazw kolumn oraz iloÅ›ci elementÃ³w w wierszach
+	 *             nie sÄ… rÃ³wne.
 	 */
-	public static <T,N extends CharSequence> String tableToString(List<N> columnNames,
-			List<List<T>> tableRows) {
-		List<Integer> columnWidths = getColumnWidths(tableRows, columnNames);
-		StringBuilder sb = new StringBuilder();
+	public static <T,N extends CharSequence> String tableToString(final List<N> columnNames,
+			final List<List<T>> tableRows) {
+		final List<Integer> columnWidths = getColumnWidths(tableRows, columnNames);
+		final StringBuilder sb = new StringBuilder();
 		if (columnNames != null) {
 			appendTableSeparator(sb, columnWidths, 1);
 			sb.append("| ");
@@ -55,7 +60,7 @@ public class Util {
 		for (int i = 0; i < tableRows.size(); i++) {
 			sb.append("| ");
 			for (int j = 0; j < tableRows.get(i).size(); j++) {
-				String cell = String.valueOf(tableRows.get(i).get(j)).trim();
+				final String cell = String.valueOf(tableRows.get(i).get(j)).trim();
 				sb.append(alignLeft(cell, columnWidths.get(j)));
 				if (j < tableRows.get(i).size() - 1) {
 					sb.append(" | ");
@@ -69,7 +74,7 @@ public class Util {
 		return sb.toString().trim();
 	}
 	private static <T,N extends CharSequence> List<Integer> getColumnWidths(
-			List<List<T>> tableRows, List<N> columnNames) {
+			final List<List<T>> tableRows, final List<N> columnNames) {
 		for (int i = 0; i < tableRows.size() - 1; i++) {
 			if (tableRows.get(i).size() != tableRows.get(i + 1).size()) {
 				throw Message.util_table_rows_uneven.illegalArgument(
@@ -77,8 +82,8 @@ public class Util {
 						i + 1, tableRows.get(i + 1).size());
 			}
 		}
-		if (columnNames != null && tableRows.size() > 0
-				&& columnNames.size() != tableRows.get(0).size()) {
+		if ((columnNames != null) && (tableRows.size() > 0)
+				&& (columnNames.size() != tableRows.get(0).size())) {
 			throw Message.util_table_rows_uneven.illegalArgument(
 					"columnNames", columnNames.size(),
 					0, tableRows.get(0).size());
@@ -89,7 +94,7 @@ public class Util {
 		} else if (tableRows.size() > 0) {
 			columnCount = tableRows.get(0).size();
 		}
-		List<Integer> columnWidths =
+		final List<Integer> columnWidths =
 			new ArrayList<Integer>(Arrays.asList(new Integer[columnCount]));
 		Collections.fill(columnWidths, 0);
 		if (columnNames != null) {
@@ -100,7 +105,7 @@ public class Util {
 		}
 		for (int i = 0; i < tableRows.size(); i++) {
 			for (int j = 0; j < tableRows.get(0).size(); j++) {
-				String cell = String.valueOf(tableRows.get(i).get(j)).trim();
+				final String cell = String.valueOf(tableRows.get(i).get(j)).trim();
 				columnWidths.set(
 						j, Math.max(columnWidths.get(j), cell.length()));
 			}
@@ -108,7 +113,7 @@ public class Util {
 		return columnWidths;
 	}
 	private static StringBuilder appendTableSeparator(
-			StringBuilder sb, List<Integer> columnWidths, int padding) {
+			final StringBuilder sb, final List<Integer> columnWidths, final int padding) {
 		sb.append("+");
 		for (int i = 0; i < columnWidths.size(); i++) {
 			for (int j = 0; j < columnWidths.get(i) + 2 * padding; j++) {
@@ -118,30 +123,30 @@ public class Util {
 		}
 		return sb.append("\n");
 	}
+
 	/**
-	 * Dopisuje spacje do napisu <code>s</code>, tak ¿e napis wynikowy ma
-	 * d³ugo¶æ <code>lineLength</code>.
-	 * Je¶li d³ugo¶æ <code>s</code> jest wiêksza ni¿ <code>lineLength</code>,
-	 * napis jest obcinany, a na koñcu dopisywane s± znaki <code>...</code>
-	 * (wielokropek) lub napis zostaje wype³niony znakami <code>*</code>,
-	 * je¶li ma postaæ liczbow±.
+	 * Dopisuje spacje do napisu <code>s</code>, tak Å¼e napis wynikowy ma
+	 * dÅ‚ugoÅ›Ä‡ <code>lineLength</code>. JeÅ›li dÅ‚ugoÅ›Ä‡ <code>s</code> jest
+	 * wiÄ™ksza niÅ¼ <code>lineLength</code>, napis jest obcinany, a na koÅ„cu
+	 * dopisywane sÄ… znaki <code>...</code> (wielokropek) lub napis zostaje
+	 * wypeÅ‚niony znakami <code>*</code>, jeÅ›li ma postaÄ‡ liczbowÄ….
 	 */
-	public static <N extends CharSequence>String alignLeft(N s, int lineLength) {
-		StringBuilder sb = new StringBuilder(s);
+	public static <N extends CharSequence>String alignLeft(final N s, final int lineLength) {
+		final StringBuilder sb = new StringBuilder(s);
 		while (sb.length() < lineLength) {
 			sb.append(' ');
 		}
 		return properCut(sb.toString(), lineLength);
 	}
+
 	/**
-	 * Je¶li d³ugo¶æ <code>s</code> jest wiêksza ni¿ <code>lineLength</code>,
-	 * napis jest obcinany, a na koñcu dopisywane s± znaki <code>...</code>
-	 * (wielokropek) lub napis zostaje wype³niony znakami <code>*</code>,
-	 * je¶li ma postaæ liczbow±.
-	 * Je¶li d³ugo¶æ <code>s</code> jest mniejsza lub równa
+	 * JeÅ›li dÅ‚ugoÅ›Ä‡ <code>s</code> jest wiÄ™ksza niÅ¼ <code>lineLength</code>,
+	 * napis jest obcinany, a na koÅ„cu dopisywane sÄ… znaki <code>...</code>
+	 * (wielokropek) lub napis zostaje wypeÅ‚niony znakami <code>*</code>, jeÅ›li
+	 * ma postaÄ‡ liczbowÄ…. JeÅ›li dÅ‚ugoÅ›Ä‡ <code>s</code> jest mniejsza lub rÃ³wna
 	 * <code>lineLength</code>, zwracany jest napis <code>s</code>.
 	 */
-	public static String properCut(String s, int lineLength) {
+	public static String properCut(final String s, final int lineLength) {
 		String result;
 		if (s.length() <= lineLength) {
 			result = s;
@@ -156,7 +161,7 @@ public class Util {
 					}
 				}
 			} else {
-				StringBuilder sb = new StringBuilder();
+				final StringBuilder sb = new StringBuilder();
 				while (sb.length() < lineLength) {
 					sb.append('*');
 				}
@@ -165,56 +170,67 @@ public class Util {
 		}
 		return result;
 	}
+
 	/**
-	 * Czy podany ci±g znaków reprezentuje liczbê.
-	 * Uwaga: brane s± pod uwagê locale, wiêc dla pl_PL liczby dziesiêtne
-	 * musz± mieæ czê¶æ u³amkow± oddzielon± przecinkiem, a nie kropk±.
+	 * Czy podany ciÄ…g znakÃ³w reprezentuje liczbÄ™. Uwaga: brane sÄ… pod uwagÄ™
+	 * locale, wiÄ™c dla pl_PL liczby dziesiÄ™tne muszÄ… mieÄ‡ czÄ™Å›Ä‡ uÅ‚amkowÄ…
+	 * oddzielonÄ… przecinkiem, a nie kropkÄ….
 	 */
-	public static boolean isNumeric(String s) {
-		NumberFormat formatter = NumberFormat.getInstance();
-		ParsePosition pos = new ParsePosition(0);
+	public static boolean isNumeric(final String s) {
+		final NumberFormat formatter = NumberFormat.getInstance();
+		final ParsePosition pos = new ParsePosition(0);
 		formatter.parse(s, pos);
 		return s.length() == pos.getIndex();
 	}
+
 	/**
-	 * Modyfikuje wiersze listy do zachowania jednakowej ilo¶ci rekordów(kolumn)
-	 * @param <T> typ list
-	 * @param rows dane wej¶ciowe
-	 * @return zmodyfikowane dane wej¶ciowe
+	 * Modyfikuje wiersze listy do zachowania jednakowej iloÅ›ci rekordÃ³w(kolumn)
+	 * 
+	 * @param <T>
+	 *            typ list
+	 * @param rows
+	 *            dane wejÅ›ciowe
+	 * @return zmodyfikowane dane wejÅ›ciowe
 	 */
-	public static <T> List<List<T>> normalize(List<List<T>> rows){
+	public static <T> List<List<T>> normalize(final List<List<T>> rows){
 		return normalize(rows, null);
 	}
-	
+
 	/**
-	 * Modyfikuje wiersze listy do zachowania jednakowej ilo¶ci rekordów(kolumn)
-	 * @param <T> typ listy
-	 * @param rows dane wej¶ciowe
-	 * @param def warto¶æ wstawiana
-	 * @return zmodyfikowane dane wej¶ciowe
+	 * Modyfikuje wiersze listy do zachowania jednakowej iloÅ›ci rekordÃ³w(kolumn)
+	 * 
+	 * @param <T>
+	 *            typ listy
+	 * @param rows
+	 *            dane wejÅ›ciowe
+	 * @param def
+	 *            wartoÅ›Ä‡ wstawiana
+	 * @return zmodyfikowane dane wejÅ›ciowe
 	 */
-	public static <T> List<List<T>> normalize(List<List<T>> rows, final T def){
+	public static <T> List<List<T>> normalize(final List<List<T>> rows, final T def){
 		int max = -1;
-		for(Collection<T> collection : rows){
+		for(final Collection<T> collection : rows){
 			max = Math.max(max, collection.size());
 		}
-		for(Collection<T> row : rows){
+		for(final Collection<T> row : rows){
 			if(row.size()<max){
 				row.addAll(Collections.nCopies(max-row.size(), def));
 			}
 		}
 		return rows;
 	}
+
 	/**
-	 * £±czy elementy listy w jeden ci±g znaków z elementami
-	 * rozdzielonymi podanym separatorem.
-	 *
-	 * @param  separator Ci±g znaków oddzielaj±cy od siebie elementy listy.
-	 * @return           Ci±g znaków z elementami rozdzielonymi separatorem.
+	 * ÅÄ…czy elementy listy w jeden ciÄ…g znakÃ³w z elementami rozdzielonymi
+	 * podanym separatorem.
+	 * 
+	 * @param separator
+	 *            CiÄ…g znakÃ³w oddzielajÄ…cy od siebie elementy listy.
+	 * @return CiÄ…g znakÃ³w z elementami rozdzielonymi separatorem.
 	 */
-	public static <T> String join(List<T> in, String separator) {
-		StringBuilder out = new StringBuilder("");
-		if (in != null && in.size() > 0) {
+	public static <T> String join(final List<T> in, final String separator) {
+		final StringBuilder out = new StringBuilder("");
+		if ((in != null) && (in.size() > 0)) {
 			for (int i = 0; i < in.size() - 1; i++) {
 				out.append(in.get(i));
 				out.append(separator);
@@ -223,49 +239,60 @@ public class Util {
 		}
 		return out.toString();
 	}
+
 	/**
-	 * £±czy elementy tablicy w jeden ci±g znaków z elementami
-	 * rozdzielonymi podanym separatorem.
-	 *
-	 * @param  separator Ci±g znaków oddzielaj±cy od siebie elementy tablicy.
-	 * @return           Ci±g znaków z elementami rozdzielonymi separatorem.
+	 * ÅÄ…czy elementy tablicy w jeden ciÄ…g znakÃ³w z elementami rozdzielonymi
+	 * podanym separatorem.
+	 * 
+	 * @param separator
+	 *            CiÄ…g znakÃ³w oddzielajÄ…cy od siebie elementy tablicy.
+	 * @return CiÄ…g znakÃ³w z elementami rozdzielonymi separatorem.
 	 */
-	public static <T> String join(T[] in, String separator) {
+	public static <T> String join(final T[] in, final String separator) {
 		return join(Arrays.asList(in), separator);
 	}
+
 	/**
 	 * Zapisuje napis do pliku
-	 * @param string napis do zapisu
-	 * @param file plik w którym nast±pi zapis
-	 * @param encoding kodowanie pliku wyj¶ciowego
+	 * 
+	 * @param string
+	 *            napis do zapisu
+	 * @param file
+	 *            plik w ktÃ³rym nastÄ…pi zapis
+	 * @param encoding
+	 *            kodowanie pliku wyjÅ›ciowego
 	 * @return status operacji
 	 */
-	public static boolean writeStringToFile(String string, File file, String encoding)
+	public static boolean writeStringToFile(final String string, final File file, final String encoding)
 	throws UnsupportedEncodingException {
 		try {
-			PrintWriter writer = new PrintWriter(new OutputStreamWriter(
+			final PrintWriter writer = new PrintWriter(new OutputStreamWriter(
 					new FileOutputStream(file), encoding));
 			writer.write(string);
 			writer.close();
 			return true;
-		} catch (FileNotFoundException e) {
+		} catch (final FileNotFoundException e) {
 			e.printStackTrace();
 			return false;
 		}
 	}
+
 	/**
 	 * Zapisuje napis do pliku
-	 * @param string napis do zapisu
-	 * @param file plik w którym nast±pi zapis
+	 * 
+	 * @param string
+	 *            napis do zapisu
+	 * @param file
+	 *            plik w ktÃ³rym nastÄ…pi zapis
 	 * @return status operacji
 	 */
-	public static boolean writeStringToFile(String string, File file) {
+	public static boolean writeStringToFile(final String string, final File file) {
 		try {
-			PrintWriter writer = new PrintWriter(new FileOutputStream(file));
+			final PrintWriter writer = new PrintWriter(new FileOutputStream(file));
 			writer.write(string);
 			writer.close();
 			return true;
-		} catch (FileNotFoundException e) {
+		} catch (final FileNotFoundException e) {
 			e.printStackTrace();
 			return false;
 		}
--- trunk/src/XLSHelper.java
+++ trunk/src/XLSHelper.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PushbackInputStream;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import mess.Message;
@@ -20,7 +19,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class XLSHelper {
 	
-	private static Workbook getWorkbook(PushbackInputStream input) throws IOException{
+	private static Workbook getWorkbook(final PushbackInputStream input) throws IOException{
 		Workbook wb = null;
 		if(POIFSFileSystem.hasPOIFSHeader(input)){
 			wb = new HSSFWorkbook(input);
@@ -32,56 +31,59 @@ public class XLSHelper {
 		return wb;
 	}
 	
-	public static List<List<Object>> read(String fileName) throws IOException{
+	public static List<List<Object>> read(final String fileName) throws IOException{
 		return read(fileName, null);
 	}
 	
-	public static List<List<Object>> read(String fileName, Object valueForNull) throws IOException{
+	public static List<List<Object>> read(final String fileName, final Object valueForNull) throws IOException{
 		return read(new FileInputStream(fileName), valueForNull);
 	}
 	
-	public static List<List<Object>> read(InputStream input) throws IOException{
+	public static List<List<Object>> read(final InputStream input) throws IOException{
 		return read(input, null);
 	}
 
 	/**
-	 * Wczytuje strumie� jako xls, xlsx
-	 * @param input strumie� plik�w xls, xlsx
-	 * @param valueForNull warto�� dla kom�rek pustych
-	 * @return lista list warto�ci, kt�re prezentuje zawarto�� kom�rek
+	 * Wczytuje strumień jako xls, xlsx
+	 * 
+	 * @param input
+	 *            strumień plików xls, xlsx
+	 * @param valueForNull
+	 *            wartość dla komórek pustych
+	 * @return lista list wartości, które prezentuje zawartość komórek
 	 * @throws IOException
 	 */
-	public static List<List<Object>> read(InputStream input, Object valueForNull) throws IOException{
-		List<List<Object>> data = new ArrayList<List<Object>>();
-		Workbook wb1 = getWorkbook(new PushbackInputStream(input, 1024));
+	public static List<List<Object>> read(final InputStream input, final Object valueForNull) throws IOException{
+		final List<List<Object>> data = new ArrayList<List<Object>>();
+		final Workbook wb1 = getWorkbook(new PushbackInputStream(input, 1024));
 		if(wb1!=null){
 			Sheet sheet;
-			int sheetCount = wb1.getNumberOfSheets();
-			List<Integer> sheets = OptionHelper.sheet.getValues();
-			List<Integer> columns = OptionHelper.column.getValues();
-			List<Integer> rows = OptionHelper.row.getValues();
+			final int sheetCount = wb1.getNumberOfSheets();
+			final List<Integer> sheets = OptionHelper.sheet.getValues();
+			final List<Integer> columns = OptionHelper.column.getValues();
+			final List<Integer> rows = OptionHelper.row.getValues();
 			sheet:for(int s = 0; s < sheetCount; s++){
-				if(sheets.size()>0 && !sheets.contains(s)){
+				if((sheets.size()>0) && !sheets.contains(s)){
 					continue sheet;
 				}
 				sheet = wb1.getSheetAt(s);
-				int firstRow = sheet.getFirstRowNum();
+				final int firstRow = sheet.getFirstRowNum();
 				row:for(int row_nr = firstRow;row_nr<sheet.getLastRowNum(); row_nr++){
-					if(rows.size()>0 && !rows.contains(row_nr)){
+					if((rows.size()>0) && !rows.contains(row_nr)){
 						continue row;
 					}
-					Row row = sheet.getRow(row_nr);
+					final Row row = sheet.getRow(row_nr);
 					if(row!=null){
-						List<Object> rowl = new ArrayList<Object>();
+						final List<Object> rowl = new ArrayList<Object>();
 						boolean add = false;
 						column:for(int cell_nr = row.getFirstCellNum(); cell_nr < row.getLastCellNum(); cell_nr++){
-							if(columns.size()>0 && !columns.contains(cell_nr)){
+							if((columns.size()>0) && !columns.contains(cell_nr)){
 								continue column;
 							}
-							Cell cell = row.getCell(cell_nr);
+							final Cell cell = row.getCell(cell_nr);
 							Object value = valueForNull;
 							if(cell!=null){
-								CellStyle style = cell.getCellStyle();
+								final CellStyle style = cell.getCellStyle();
 								if(style.getHidden()){
 									continue;
 								}
